55a46bf7f138b20ba66aad16873484f626fc993c
Ward McKinley <wmck126@gmail.com>
2022-12-12 16:23:35 -0500

    routes setup

---
+++ cocktails/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <p>wow this is a lot of nothing</p>
+    </div>
+  )
+}
+
+export default HomePage
